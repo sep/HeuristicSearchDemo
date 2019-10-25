a60753ced8909e1a26eee8470285c3fb7b85c600
Jordan Thayer <jtthayer@sep.com>
2019-10-25 08:15:22 -0400

    Defines Grid Search Domain in part
    Grid domain is a great testing tool because it's easy to visualize and understand.

---
+++ HeuristicSearch/GridNavigation.fs
@@ -0,0 +1,56 @@
+﻿module GridNavigation
+
+type Action =
+| North
+| South
+| East
+| West
+
+type Position = {
+    x : int
+    y: int
+}
+
+type Board = bool [,]
+
+type Problem = {
+    start : Position
+    finish : Position
+    board : Board
+}
+
+let goal_test (problem : Problem) (position : Position) =
+    problem.finish = position
+
+let move (position : Position) = function
+| North -> { x = position.x; y = position.y - 1 }
+| South -> { x = position.x; y = position.y + 1 }
+| East -> { x = position.x + 1; y = position.y }
+| West -> { x = position.x - 1; y = position.y }
+ 
+let legal_position (board : Board) (position : Position) =
+    position.x > 0 && 
+    position.y > 0 && 
+    position.x < (board.GetLength 0) && 
+    position.y < (board.GetLength 1) &&
+    board.[position.x, position.y]
+
+let expand (board : Board) (position : Position) = 
+    let possible_actions = [North; South; East; West]
+    let validate_position = legal_position board
+    let consider_action (accum : (Position * float) list) (action : Action) = 
+        let position' = move position action
+        if validate_position position' then
+                (position', 1.) :: accum
+            else
+                accum
+
+    List.fold consider_action [] possible_actions
+
+// todo: perfect hash by linearizing based on board size
+let key (position : Position) =
+    position.x, position.y
+
+//(initial_state : 'state)
+
+
--- HeuristicSearchTests/UnitTest1.fs
+++ HeuristicSearchTests/UnitTest1.fs
@@ -8,4 +8,9 @@ let Setup () =
 
 [<Test>]
 let Test1 () =
-    Assert.Pass()
+    let (board : GridNavigation.Board) = Array2D.create 3 3 true
+    let (start : GridNavigation.Position) = { x = 0; y = 0 }
+    let (finish : GridNavigation.Position) = { x = 2; y = 2 }
+    let (problem : GridNavigation.Problem) = { start = start; finish = finish; board = board }
+
+    0
