ede5904fbedb439c71945828622e2c729ac1968f
Jordan Thayer <jtthayer@sep.com>
2019-10-28 14:52:48 -0400

    Use mutable over ref declarations
    Mutable attempts to do a stack allocation; the compiler will implicitly convert to a ref cell "when needed". If we need explicit control over allocation in the future, this may need to change.

--- HeuristicSearch/UniformCostSearch.fs
+++ HeuristicSearch/UniformCostSearch.fs
@@ -66,38 +66,38 @@ let generate_solution_of_sol_node (sol_node : ('a SearchNode) SolutionNode) =
     generate_solution sol_node.solution
 
 let uniform_cost_search (expand : 'state -> ('state * float) list) (goal_test : 'state -> bool) (key : 'state -> 'hash_value) (initial_state : 'state) =
-    let (openlist : Ref<FSharpx.Collections.IPriorityQueue<'state SearchNode>>) = ref (FSharpx.Collections.PriorityQueue.empty false)
-    let closedlist = ref Map.empty
+    let mutable openlist : FSharpx.Collections.IPriorityQueue<'state SearchNode> = FSharpx.Collections.PriorityQueue.empty false
+    let mutable closedlist = Map.empty
     let root = make_root initial_state
     let metrics = initial_metrics ()
     let node_key = wrap_state_fn key
     let node_expand = wrap_state_fn expand
     let node_goal_test = wrap_state_fn goal_test
-    let enqueue (node : 'state SearchNode) = openlist := FSharpx.Collections.PriorityQueue.insert node !openlist
+    let enqueue (node : 'state SearchNode) = openlist <- FSharpx.Collections.PriorityQueue.insert node openlist
     let consider_child current_node (state, step_cost) = 
         let child_node = { parent = Some current_node; state = state; cost = current_node.cost + step_cost }
         match metrics.solution_nodes with
             | [] -> enqueue child_node
             | hd::_ -> if better child_node hd.solution then enqueue child_node
     let pop () =
-        match FSharpx.Collections.PriorityQueue.tryPop !openlist with
-        | Some (ret, openlist') -> openlist := openlist'; Some ret
+        match FSharpx.Collections.PriorityQueue.tryPop openlist with
+        | Some (ret, openlist') -> openlist <- openlist'; Some ret
         | _ -> None
     enqueue root
-    let finished = ref false
-    while not !finished do
+    let mutable finished = false
+    while not finished do
         match pop() with
-        | None -> finished := true
+        | None -> finished <- true
         | Some current_node -> 
             if node_goal_test current_node then begin
                 metrics.solution_nodes <- { solution = current_node; found_at_time = DateTime.Now } :: metrics.solution_nodes;
-                finished := true end else
+                finished <- true end else
                 begin
                     let key_val = node_key current_node
-                    if Map.containsKey key_val !closedlist then
+                    if Map.containsKey key_val closedlist then
                         metrics.duplicates <- metrics.duplicates + 1
                     else begin
-                        closedlist := Map.add key_val current_node.cost !closedlist
+                        closedlist <- Map.add key_val current_node.cost closedlist
                         let child_tuples = node_expand current_node
                         List.iter (consider_child current_node) child_tuples
                     end
