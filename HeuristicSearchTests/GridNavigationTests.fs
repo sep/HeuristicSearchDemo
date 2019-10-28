d470f8d760eff24e559cf232019a28a69bae0bfb
Jordan Thayer <jtthayer@sep.com>
2019-10-28 12:34:55 -0400

    Pulled apart domain, search algorithm tests into separate files

--- HeuristicSearchTests/GridNavigationTests.fs
+++ HeuristicSearchTests/GridNavigationTests.fs
@@ -1,4 +1,4 @@
-module HeuristicSearchTests
+module DomainTests
 
 open NUnit.Framework
 open Microsoft.FSharp.Reflection
@@ -109,23 +109,6 @@ let InvalidSolutionFalse () =
     ]
     Assert.False (GN.validate_solution problem solution)
 
-[<Test>]
-let EndToEnd () =
-    let (board : GN.Board) = Array2D.create 3 3 true
-    board.[1,1] <- false
-    let (start : GN.Position) = { x = 0; y = 0 }
-    let (finish : GN.Position) = { x = 2; y = 2 }
-    let (problem : GN.Problem) = { start = start; finish = finish; board = board }
-    let (root : GN.State) = GN.make_initial_state problem
-    let expand = GN.expand problem.board
-    let key = GN.key problem.board
-    let goal = GN.goal_test problem in
-    let metrics = UCS.uniform_cost_search expand goal key root in
-    let generate_solution (node : (GN.State UCS.SearchNode) UCS.SolutionNode) = UCS.generate_solution_of_sol_node node
-    let validate_sol_node (node : (GN.State UCS.SearchNode) UCS.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
-        GN.problem_to_string problem |> printf "%s\n";
-        Assert.AreEqual(2, metrics.solution_nodes.Length);
-        List.fold (fun accum e -> accum && validate_sol_node e) true metrics.solution_nodes |> Assert.True
 
 [<Test>]
 let RandomProblemsAreValid() =
---
+++ HeuristicSearchTests/UniformCostSearchTests.fs
@@ -0,0 +1,29 @@
+module HeuristicSearchTests
+
+open NUnit.Framework
+open Microsoft.FSharp.Reflection
+module GN = GridNavigation
+module UCS = UniformCostSearch
+
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let EndToEnd () =
+    let (board : GN.Board) = Array2D.create 3 3 true
+    board.[1,1] <- false
+    let (start : GN.Position) = { x = 0; y = 0 }
+    let (finish : GN.Position) = { x = 2; y = 2 }
+    let (problem : GN.Problem) = { start = start; finish = finish; board = board }
+    let (root : GN.State) = GN.make_initial_state problem
+    let expand = GN.expand problem.board
+    let key = GN.key problem.board
+    let goal = GN.goal_test problem in
+    let metrics = UCS.uniform_cost_search expand goal key root in
+    let generate_solution (node : (GN.State UCS.SearchNode) UCS.SolutionNode) = UCS.generate_solution_of_sol_node node
+    let validate_sol_node (node : (GN.State UCS.SearchNode) UCS.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
+        GN.problem_to_string problem |> printf "%s\n";
+        Assert.AreEqual(2, metrics.solution_nodes.Length);
+        List.fold (fun accum e -> accum && validate_sol_node e) true metrics.solution_nodes |> Assert.True
