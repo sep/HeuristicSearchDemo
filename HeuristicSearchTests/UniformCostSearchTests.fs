2b1f18d6f49187586bfae407e73e7c0fe5c10c05
Jordan Thayer <jtthayer@sep.com>
2020-01-02 09:39:43 -0500

    Adds end to end test for string edit distance for uniform cost search

--- HeuristicSearchTests/UniformCostSearchTests.fs
+++ HeuristicSearchTests/UniformCostSearchTests.fs
@@ -2,6 +2,7 @@ module HeuristicSearchTests
 
 open NUnit.Framework
 module GN = GridNavigation
+module SED = StringEditDistance
 module SI = SearchInterface
 module UCS = UniformCostSearch
 
@@ -26,4 +27,18 @@ let EndToEnd_GridNavigation () =
     let validate_sol_node (node : (GN.State UCS.SearchNode) SI.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
         GN.problem_to_string problem |> printf "%s\n";
         Assert.AreEqual(1, metrics.solution_nodes.Length);
+        List.fold (fun accum e -> accum && validate_sol_node e) true metrics.solution_nodes |> Assert.True
+
+[<Test>]
+let EndToEnd_StringEditing () =
+    let (problem : SED.Problem) = SED.instance_of_strings "HERE" "THERE"
+    let (root : SED.State) = SED.make_initial_state problem
+    let expand = SED.expand problem
+    let key = SED.key
+    let goal = SED.goal_test problem in
+    let metrics = UCS.uniform_cost_search expand goal key root in
+    let generate_solution (node : (SED.State UCS.SearchNode) SI.SolutionNode) = UCS.generate_solution_of_sol_node node
+    let validate_sol_node (node : (SED.State UCS.SearchNode) SI.SolutionNode) = generate_solution node |> (SED.validate_solution problem) in
+        Assert.AreEqual(1, metrics.solution_nodes.Length);
+        List.iter SED.print_solution (List.map generate_solution metrics.solution_nodes);
         List.fold (fun accum e -> accum && validate_sol_node e) true metrics.solution_nodes |> Assert.True
