42c855451a1db015bc4145fd997f2c49db463f8c
Jordan Thayer <jtthayer@sep.com>
2020-01-03 08:57:08 -0500

    Convert A* to run on domain interfaces

--- HeuristicSearchTests/AstarSearchTests.fs
+++ HeuristicSearchTests/AstarSearchTests.fs
@@ -16,12 +16,8 @@ let EndToEnd_GridNavigation () =
     let (start : GN.Position) = { x = 0; y = 0 }
     let (finish : GN.Position) = { x = 2; y = 2 }
     let (problem : GN.Problem) = { start = start; finish = finish; board = board }
-    let (root : GN.State) = GN.make_initial_state problem
-    let expand = GN.expand problem.board
-    let key = GN.key problem.board
-    let goal = GN.goal_test problem in
-    let heuristic state = GN.manhattan_distance problem state |> float
-    let metrics = Astar.astar_search expand goal key heuristic root in
+    let iface = GN.GridDomainInterface(problem)
+    let metrics = Astar.astar_search iface in
     let generate_solution (node : (GN.State Astar.SearchNode) SI.SolutionNode) = Astar.generate_solution_of_sol_node node
     let validate_sol_node (node : (GN.State Astar.SearchNode) SI.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
         GN.problem_to_string problem |> printf "%s\n";
@@ -31,12 +27,8 @@ let EndToEnd_GridNavigation () =
 [<Test>]
 let Benchmark_GridNavigation () =
     let problem = GN.random_problem None 2000 2000 0.25 in
-    let (root : GN.State) = GN.make_initial_state problem
-    let expand = GN.expand problem.board
-    let key = GN.key problem.board
-    let goal = GN.goal_test problem in
-    let heuristic state = GN.manhattan_distance problem state |> float
-    let metrics = Astar.astar_search expand goal key heuristic root in
+    let iface = GN.GridDomainInterface(problem)
+    let metrics = Astar.astar_search iface in
     let generate_solution (node : (GN.State Astar.SearchNode) SI.SolutionNode) = Astar.generate_solution_of_sol_node node
     let validate_sol_node (node : (GN.State Astar.SearchNode) SI.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
         Assert.AreEqual(1, metrics.solution_nodes.Length);
@@ -46,12 +38,8 @@ let Benchmark_GridNavigation () =
 [<Test>]
 let EndToEnd_StringEditing () =
     let (problem : SED.Problem) = SED.instance_of_strings "HERE" "THERE"
-    let (root : SED.State) = SED.make_initial_state problem
-    let expand = SED.expand problem
-    let key = SED.key
-    let goal = SED.goal_test problem in
-    let heuristic = SED.character_delta problem
-    let metrics = Astar.astar_search expand goal key heuristic root in
+    let iface = SED.StringDomainInterface(problem)
+    let metrics = Astar.astar_search iface in
     let generate_solution (node : (SED.State Astar.SearchNode) SI.SolutionNode) = Astar.generate_solution_of_sol_node node
     let validate_sol_node (node : (SED.State Astar.SearchNode) SI.SolutionNode) = generate_solution node |> (SED.validate_solution problem) in
         Assert.AreEqual(1, metrics.solution_nodes.Length);
