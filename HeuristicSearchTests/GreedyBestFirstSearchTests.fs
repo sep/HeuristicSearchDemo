b327f919fb6a7fe80f5b5efe9ab1787651fae674
Jordan Thayer <jtthayer@sep.com>
2020-01-03 08:53:16 -0500

    Rewrites Greedy Best first search in terms of interface

--- HeuristicSearchTests/GreedyBestFirstSearchTests.fs
+++ HeuristicSearchTests/GreedyBestFirstSearchTests.fs
@@ -17,12 +17,8 @@ let EndToEnd_GridNavigation () =
     let (start : GN.Position) = { x = 0; y = 0 }
     let (finish : GN.Position) = { x = 2; y = 2 }
     let (problem : GN.Problem) = { start = start; finish = finish; board = board }
-    let heuristic state = GN.manhattan_distance problem state |> float
-    let (root : GN.State) = GN.make_initial_state problem
-    let expand = GN.expand problem.board
-    let key = GN.key problem.board
-    let goal = GN.goal_test problem in
-    let metrics = GBFS.greedy_best_first_search expand goal key heuristic root in
+    let iface = GN.GridDomainInterface(problem)
+    let metrics = GBFS.greedy_best_first_search iface in
     let generate_solution (node : (GN.State GBFS.SearchNode) SI.SolutionNode) = GBFS.generate_solution_of_sol_node node
     let validate_sol_node (node : (GN.State GBFS.SearchNode) SI.SolutionNode) = generate_solution node |> (GN.validate_solution problem) in
         GN.problem_to_string problem |> printf "%s\n";
@@ -33,12 +29,8 @@ let EndToEnd_GridNavigation () =
 [<Test>]
 let EndToEnd_StringEditing () =
     let (problem : SED.Problem) = SED.instance_of_strings "HERE" "THERE"
-    let heuristic = SED.character_delta problem
-    let (root : SED.State) = SED.make_initial_state problem
-    let expand = SED.expand problem
-    let key = SED.key
-    let goal = SED.goal_test problem in
-    let metrics = GBFS.greedy_best_first_search expand goal key heuristic root in
+    let iface = SED.StringDomainInterface(problem)
+    let metrics = GBFS.greedy_best_first_search iface in
     let generate_solution (node : (SED.State GBFS.SearchNode) SI.SolutionNode) = GBFS.generate_solution_of_sol_node node
     let validate_sol_node (node : (SED.State GBFS.SearchNode) SI.SolutionNode) = generate_solution node |> (SED.validate_solution problem) in
         Assert.AreEqual(1, metrics.solution_nodes.Length);
